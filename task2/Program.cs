ce814fb191dd4561ef7e11121eea9b2e4e4b7791
alex_di <mirag0505@yandex.ru>
2023-08-31 12:39:12 +0300

    done scratch for 2 task

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿using AlgorithmsDataStructures2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
---
+++ testTask2/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace testTask2;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
