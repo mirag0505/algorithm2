969baa0860638891a12547721d69f89eb1d39835
alex_di <mirag0505@yandex.ru>
2023-09-24 13:16:44 +0300

    adee scratch for 4 tasks

---
+++ task4/BalancedBST.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            return null;
+        }
+    }
+}
---
+++ testTask4/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace testTask4;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
